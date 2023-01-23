ba911d91c8fe88cf88ea66c048da59353250d673
makvearti <makvearti@gmail.com>
2023-01-24 00:10:39 +0300

    24.01.23

--- Web/ViewModels/ErrorViewModel.cs
+++ Web/ViewModels/ErrorViewModel.cs
@@ -2,5 +2,7 @@ namespace Web.ViewModels;
 
 public class ErrorViewModel
 {
-    public string RequestId {get;set;}
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
