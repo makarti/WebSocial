ba6788650dcd9a40a177ea57175c795c16a96fec
Makvetsyan Artur <makvetsyan.arti@gmail.com>
2023-03-20 12:23:54 +0300

    docker compose

--- Common/Core/Utils/DBMySqlUtils.cs
+++ Common/Core/Utils/DBMySqlUtils.cs
@@ -4,9 +4,17 @@ namespace Core.Utils;
 
 public class DBMySqlUtils
 {
-    public static MySqlConnection GetDBConnection(string host, int port, string database, string username, string password)
+    public static MySqlConnection GetDBConnection(string host, string database, string password)
     {
-        string connString = $"Server=\"{host}\";Database=\"{database}\";port=\"{port}\";user=\"{username}\";password=\"{password}\"";
+        string connString = $"Server={host};Database={database};port=3306;user=root;password={password}";
+
+        MySqlConnection conn = new MySqlConnection(connString);
+        
+        return conn;
+    }
+    public static MySqlConnection GetDBConnection(string host, string password)
+    {
+        string connString = $"Server={host};port=3306;user=root;password={password}";
 
         MySqlConnection conn = new MySqlConnection(connString);
         
--- Common/Core/Utils/DBUtils.cs
+++ Common/Core/Utils/DBUtils.cs
@@ -1,23 +1,79 @@
 using Core.Configuration;
+using Core.Entities;
+using Dapper;
 using MySql.Data.MySqlClient;
 
 namespace Core.Utils;
 
 public class DBUtils
 {
+    private static string _host;
+    private static string _database;
+    private static string _password;
+
+    static DBUtils()
+    {
+        _host = Environment.GetEnvironmentVariable("DB_HOST");
+        _database = Environment.GetEnvironmentVariable("DB_NAME");
+        _password = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD");
+
+        Initialize();
+    }  
+
     public static MySqlConnection GetDBConnection()
     {
-        string host = ConfigurationManager.AppSetting["ConnectionStrings:Host"];
-        int port = int.Parse(ConfigurationManager.AppSetting["ConnectionStrings:Port"]);
-        string database = ConfigurationManager.AppSetting["ConnectionStrings:Database"];
-        string username = ConfigurationManager.AppSetting["ConnectionStrings:UserName"];
-        string password = ConfigurationManager.AppSetting["ConnectionStrings:Password"];
-        //string host = "localhost";
-        //int port = 3306;
-        //string database = "WebSocial";
-        //string username = "root";
-        //string password = "Password123#@!";
-
-        return DBMySqlUtils.GetDBConnection(host, port, database, username, password);        
+        return DBMySqlUtils.GetDBConnection(_host, _database, _password);        
     }
+
+    #region initialize db
+
+    private static void Initialize()
+    {
+        InitializeDatabase();
+        InitializeTables();
+    }
+
+    private static void InitializeDatabase()
+    {
+        const string sql = @"CREATE DATABASE IF NOT EXISTS `WebSocial`;";
+
+        using (var connection = DBMySqlUtils.GetDBConnection(_host, _password))
+        {
+            connection.Execute(sql);
+        }
+    }
+
+    private static void InitializeTables()
+    {
+        const string sqltables = @"
+                            CREATE TABLE WebSocial.`Account` (
+                              `Id` varchar(36) NOT NULL,
+                              `Login` varchar(45) NOT NULL,
+                              `Password` varchar(100) NOT NULL,
+                              `FirstName` varchar(45) NOT NULL,
+                              `LastName` varchar(45) NOT NULL,
+                              `Age` tinyint NOT NULL,
+                              `Gender` tinyint(1) NOT NULL,
+                              `Interests` varchar(255) NOT NULL,
+                              `City` varchar(100) NOT NULL,
+                              `CreateDate` datetime NOT NULL,
+                              PRIMARY KEY (`Id`),
+                              UNIQUE KEY `Id_UNIQUE` (`Id`),
+                              UNIQUE KEY `Login_UNIQUE` (`Login`)
+                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+                            CREATE TABLE WebSocial.`Friendship` (
+                              `RequesterId` varchar(36) NOT NULL,
+                              `AddresserId` varchar(36) NOT NULL,
+                              `Created` datetime NOT NULL,
+                              `Status` tinyint(1) NOT NULL DEFAULT (0),
+                              PRIMARY KEY (`RequesterId`,`AddresserId`)
+                            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+        using (var connection = DBMySqlUtils.GetDBConnection(_host, _database, _password))
+        {
+            connection.Execute(sqltables);
+        }
+    }
+    #endregion
 }
